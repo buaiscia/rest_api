cfe7b162521c9bcb82e72d350a663b80becaa3f0
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2020-02-20 14:40:53 +0100

    setup cors

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const Joi = require('@hapi/joi');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json());
 
+//SETUP CORS
+
+const  allowedOrigins = ['http://localhost:3000', 
+                         'http://localhost:4000'];
+
+app.use(cors({
+    origin: function(origin, callback) {
+        if(!origin) return callback(null, true);
+        if(allowedOrigins.indexOf(origin) === -1) {
+            const msg = 'The CORS policy for this site does not allow access from the specified origin'
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
 
 // SET ROUTES
 const indexRoute = require("./routes/index");

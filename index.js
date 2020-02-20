aaa2752563c3952627c2da120148a1e6999da927
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2020-02-20 11:49:16 +0100

    initial get structure

--- index.js
+++ index.js
@@ -2,16 +2,30 @@ const express = require('express');
 
 const app = express();
 
+const movies = [
+    { id: 1, name: 'movie1'},
+    { id: 2, name: 'movie2'},
+    { id: 3, name: 'movie3'},
+]
+
 app.get('/', (req, res) => {
     res.send('Hello world');
 });
 
 app.get('/api/movies/', (req, res) => {
-    res.send([1,2,3]);
+    res.send(courses);
+});
+
+
+app.get('/api/movies/:id', (req, res) => {
+    const movies = movies.find(c => c.id === parseInt(req.params.id));
+    if(!movies) { //404
+        res.status(404).send('The movie with the given id was not found')
+    }
+    res.send(movies);
 });
 
 
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port} ...`)
-)
+app.listen(port, () => console.log(`Listening on port ${port} ...`));

b6cf3e2488b5c4a59895dd69686591278f762d12
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2020-02-20 14:10:19 +0100

    add 404

--- server.js
+++ server.js
@@ -5,13 +5,18 @@ const app = express();
 
 app.use(express.json());
 
+
+// SET ROUTES
 const indexRoute = require("./routes/index");
 
+// USE ROUTES
 
 app.use('/', indexRoute)
 
 
-
+app.get('*', function(req, res) {
+    res.status(404).send('Page not found');
+});
 
 
 

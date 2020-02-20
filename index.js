6a552da7b3e2c054315cc59c79c5aa6253ab8ae6
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2020-02-20 11:47:49 +0100

    add ex get movies and listening

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 const express = require('express');
 
 const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello world');
+});
+
+app.get('/api/movies/', (req, res) => {
+    res.send([1,2,3]);
+});
+
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port} ...`)
+)

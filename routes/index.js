47fe55ba3fa5adf37f7cbbad721502df261d254e
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2020-02-28 21:35:18 +0100

    refactored all endpoints + changed PUT for PATCH +  cleaned up dummy code

--- routes/index.js
+++ routes/index.js
@@ -10,21 +10,10 @@ const app = express();
 app.use(express.json());
 
 
-const movies = [
-    // { id: 1, title: 'movie1' },
-    // { id: 2, title: 'movie2' },
-    // { id: 3, title: 'movie3' },
-]
-
-
 function validateMovie(movie) {
     const schema = Joi.object({
         title: Joi.string().min(2).required()
     });
-
-
-    // check if movie validates fields, or return 400 invalid
-
     return schema.validate(movie);
 }
 
@@ -36,8 +25,6 @@ router.get('/', (req, res, next) => {
 });
 
 router.get('/movies/', (req, res, next) => {
-    // if(movies.length > 0) res.send(movies);
-    // else res.send('movies not found');
 
     Movie.find()
         .exec()
@@ -57,12 +44,6 @@ router.get('/movies/', (req, res, next) => {
 
 
 router.get('/movies/:id', (req, res, next) => {
-    // const movie = movies.find(c => c.id === parseInt(req.params.id));
-    // if (!movie) { //404
-    //    return res.status(404).send('The movie with the given id was not found')
-    // }
-    // res.send(movie);
-
     const id = req.params.id;
     Movie.findById(id)
         .then(result => {
@@ -81,8 +62,6 @@ router.get('/movies/:id', (req, res, next) => {
 
 router.post('/movies', (req, res, next) => {
 
-    // console.log(req.body);
-
 
     const movie = new Movie({
         _id: new mongoose.Types.ObjectId(),
@@ -94,117 +73,36 @@ router.post('/movies', (req, res, next) => {
         releaseDate: req.body.releaseDate,
         images: req.body.images,
         genre: req.body.genre,
-        // genre: {
-        //     id: req.body.genre.id,
-        //     name: req.body.genre.name
-        // },
         childrenFriendly: req.body.childrenFriendly
     });
 
-    movie
-        .save()
-        .exec()
+    movie.save()
         .then(result => {
             res.status(200).send(result);
         })
         .catch(err => {
             res.status(500).json({ status: 500, message: err.message });
         })
-
-
-    // const { error } = validateMovie(req.body);
-
-    // if (error) {
-    //     return res.status(400).send(error.details[0].message);
-
-    // }
-
-    // const movie = {
-    //     id: movies.length + 1,
-    //     title: req.body.title
-    // };
-    // movies.push(movie);
-    // res.send(movie);
 });
 
 
-router.put('/movies/:id', (req, res, next) => {
-
-    // Movie.findById(req.params.id, function (err, movie) {
-    //     if (err) {
-    //         return res.status(500).json({ status: 500, message: err.message });
-    //     }
-    //     movie.title = req.body.title;
-    //     movie.director = req.body.director;
-    //     movie.description = req.body.description;
-    //     movie.shortDescription = req.body.shortDescription;
-    //     movie.duration = req.body.duration;
-    //     movie.releaseDate = req.body.releaseDate;
-    //     movie.images = req.body.images;
-    //     movie.genre = req.body.genre;
-    //     movie.childrenFriendly = req.body.childrenFriendly;
-    //     movie.save(function (err) {
-    //         if (err) {
-    //             return res.status(500).json({ status: 500, message: err.message });
-    //         }
-    //         res.status(200).send(movie);
-    //     });
-    // });
-
+router.patch('/movies/:id', (req, res, next) => {
     const id = req.params.id;
-    Movie.findById(id)
-        .then(movie => {
-            movie.title = req.body.title;
-            movie.director = req.body.director;
-            movie.description = req.body.description;
-            movie.shortDescription = req.body.shortDescription;
-            movie.duration = req.body.duration;
-            movie.releaseDate = req.body.releaseDate;
-            movie.images = req.body.images;
-            movie.genre = req.body.genre;
-            movie.childrenFriendly = req.body.childrenFriendly;
-            movie.save()
-                .then(result => {
-                    res.status(200).send(movie);
-                })
-                .catch(err => {
-                    res.status(500).json({ status: 500, message: err.message });
-                })
+
+    Movie.update({ _id: id }, req.body)
+        .then(result => {
+            res.status(200).send(result);
         })
         .catch(err => {
             res.status(500).json({ status: 500, message: err.message });
         })
+})
 
-
-
-    // const movie = movies.find(c => c.id === parseInt(req.params.id));
-    // if (!movie) { //404
-    //     return res.status(404).send('The movie with the given id was not found')
-    // }
-
-    // const { error } = validateMovie(req.body);
-
-    // if (error) {
-    //     return res.status(400).send(error.details[0].message);
-    // }
-
-    // movie.title = req.body.title;
-    // res.send(movie);
-});
 
 router.delete('/movies/:id', (req, res, next) => {
 
-    // Movie.findById(req.params.id, function (err, movie) {
-    //     movie.remove(function (err, movie) {
-    //         if (err) {
-    //             return res.status(500).json({ status: 500, message: err.message });
-    //         }
-    //         res.status(200).json({ status: 200, message: "Movie deleted" })
-    //     });
-    // });
-
     const id = req.params.id;
-    Movie.remove({ _id: id })
+    Movie.deleteOne({ _id: id })
         .exec()
         .then(result => {
             res.status(200).json({ status: 200, deletedMovie: result, message: "Movie deleted" })
@@ -212,19 +110,6 @@ router.delete('/movies/:id', (req, res, next) => {
         .catch(err => {
             res.status(500).json({ status: 500, message: err.message });
         })
-
-
-
-
-    // const movie = movies.find(c => c.id === parseInt(req.params.id));
-    // if (!movie) { //404
-    //     return res.status(404).send('The movie with the given id was not found')
-    // }
-
-    // const index = movies.indexOf(movie); //take index in array to find
-    // movies.splice(index, 1);    // delete movie from array
-
-    // res.send(movie);
 });
 
 

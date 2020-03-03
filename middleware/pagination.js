37ca4f2d4c5de592367b5995955ccd070cc87d14
Alex Buaiscia (HCI) <alex.buaiscia@gmail.com>
2020-03-03 11:28:57 +0100

    fixed pagination for queries + added escape query string check

--- middleware/pagination.js
+++ middleware/pagination.js
@@ -1,5 +1,8 @@
 
 
+const escapeStringRegexp = require('escape-string-regexp');
+
+
 exports.paginatedResults = (model) => {
     return async (req, res, next) => {
         const page = parseInt(req.query.page);
@@ -23,27 +26,30 @@ exports.paginatedResults = (model) => {
         }
 
 
-
         try {
-            if (req.query.search) {
-                const search_key = req.query.search;
-                results.results = await model
-                    .find({ title: new RegExp(search_key, 'i') })
-                    .select('-__v')
-                    .limit(limit)
-                    .skip(startIndex)
-                    .exec();
-            }
-            if (req.query.genre) {
-                const genre_key = req.query.genre;
-                results.results = await model
-                    .find({ genre: new RegExp(genre_key, 'i') })
-                    .select('-__v')
-                    .limit(limit)
-                    .skip(startIndex)
-                    .exec();
+            results.results = {}
 
+            if (req.query.search || req.query.genre) {
+                if (req.query.search) {
+                    const search_key = escapeStringRegexp(req.query["search"].toString());
+                    results.results = await model
+                        .find({ title: new RegExp(search_key, 'i') })
+                        .select('-__v')
+                        .limit(limit)
+                        .skip(startIndex)
+                        .exec();
+                }
+                else {
+                    const genre_key = escapeStringRegexp(req.query["genre"].toString());
+                    results.results = await model
+                        .find({ genre: new RegExp(genre_key, 'i') })
+                        .select('-__v')
+                        .limit(limit)
+                        .skip(startIndex)
+                        .exec();
+                }
             }
+
             else {
                 results.results = await model
                     .find().select('-__v')
@@ -52,15 +58,20 @@ exports.paginatedResults = (model) => {
                     .exec();
             }
 
-            if (Object.keys(results.results).length === 0) {
-                res.status(404).json({ status: 404, message: 'Not found' });
+            try {
+                if (Object.keys(results.results).length === 0) {
+                    res.status(404).json({ status: 404, message: 'Not found' });
+                }
+                else {
+                    totalCount = await results.results.length;
+                    results.countFound = totalCount;
+                    res.paginatedResults = results;
+                    next()
+                }
+            } catch (error) {
+                res.status(500).json({ message: error.message })
             }
-            else {
-                totalCount = await results.results.length;
-                results.countFound = totalCount;
-                res.paginatedResults = results;
-                next()
-            }
+
 
         } catch (error) {
             res.status(500).json({ message: error.message })
